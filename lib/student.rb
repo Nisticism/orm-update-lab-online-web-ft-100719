ad75e9d2162d44d3ca9819b713644998e05076cc
Hans Foster <fosterhans@gmail.com>
2019-11-21 22:47:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,7 @@ class Student
   end
   
   def self.new_from_db(row)
-    new_student = Student.new(row[0], row[1], row[2])
+    new_student = Student.new(row[2], row[0], row[1])
     new_student
   end
   
